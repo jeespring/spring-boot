c0d0c4d97a8ad552aed94c91d388b19fb77536c7
张朝阳 <jeespring@163.com>
2022-03-17 23:46:17 +0800

    Spring Boot Kafka

---
+++ spring-boot-kafka/src/main/java/com/jeespring/kafka/KafkaApplication.java
@@ -0,0 +1,18 @@
+package com.jeespring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description KafkaApplication
+ * @author 张朝阳
+ * @date 2022/3/1 1:09
+ */
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
