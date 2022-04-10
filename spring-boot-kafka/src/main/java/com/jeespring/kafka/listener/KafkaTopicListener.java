29abf316c1aa91fce478bfdce3ce23612b70fe6f
张朝阳 <jeespring@163.com>
2022-04-10 21:43:08 +0800

    Spring Boot Kafka

--- spring-boot-kafka/src/main/java/com/jeespring/kafka/listener/KafkaTopicListener.java
+++ spring-boot-kafka/src/main/java/com/jeespring/kafka/listener/KafkaTopicListener.java
@@ -1,5 +1,6 @@
 package com.jeespring.kafka.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,12 @@ import org.springframework.stereotype.Component;
  * @author 张朝阳
  * @date 2022/4/10 13:03:03
  */
+@Slf4j
 @Component
 public class KafkaTopicListener {
 
     @KafkaListener(topics = {"order-topic"})
     public void receiveMessage(String message){
-        System.out.println(message);
+        log.info("接收到的消息：{}" , message);
     }
 }
