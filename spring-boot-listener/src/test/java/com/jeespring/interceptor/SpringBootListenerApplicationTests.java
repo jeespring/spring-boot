d3274298d36e856ba82f4d54fbde62d6c973e3a3
张朝阳 <jeespring@163.com>
2022-04-20 00:19:58 +0800

    Spring Boot Listener

---
+++ spring-boot-listener/src/test/java/com/jeespring/interceptor/SpringBootListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeespring.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
