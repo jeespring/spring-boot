ada3acc39ddb6709d5443a39c2757873e5d8d36c
张朝阳 <jeespring@163.com>
2022-04-23 15:01:51 +0800

    Spring Boot FreeMarker

---
+++ spring-boot-freemarker/src/main/java/com/jeespring/freemarker/FreeMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.jeespring.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeMarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMarkerApplication.class, args);
+    }
+
+}
---
+++ spring-boot-freemarker/src/test/java/com/jeespring/interceptor/FreeMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeespring.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeMarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
