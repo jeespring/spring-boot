30ffb841b1b3d4b814378bbc203026691353ebe4
张朝阳 <jeespring@163.com>
2023-02-07 23:07:35 +0800

    Spring Boot Flowable

---
+++ spring-boot-flowable/src/test/java/com/jeespring/flowable/FlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeespring.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
